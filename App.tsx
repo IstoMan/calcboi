070c1e3312b496ade388ce755d51ab74d28f3e0d
IstoMan <suhasnaraparaju@gmail.com>
2026-04-01 10:58:48 +0530

    simplified the template

--- App.tsx
+++ App.tsx
@@ -1,14 +1,16 @@
-import { ScreenContent } from 'components/ScreenContent';
 import { StatusBar } from 'expo-status-bar';
 
 import './global.css';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </SafeAreaProvider>
+      <SafeAreaView className='flex-1 justify-center items-center'>
+        <Text className='text-[90px] text-red-500'>Hello App</Text>
+      </SafeAreaView>
+      <StatusBar style='auto'></StatusBar>
+    </SafeAreaProvider >
   );
 }
